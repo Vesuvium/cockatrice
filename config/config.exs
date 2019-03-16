5868b998e705a29ed2f8ede720e0d71163bdf465
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-03-16 19:53:17 +0100

    chore/config: add port to config

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :cockatrice,
-  dist: {:system, "COCKATRICE_DIST_FOLDER", "dist"}
+  dist: {:system, "COCKATRICE_DIST_FOLDER", "dist"},
+  port: {:system, "COCKATRICE_PORT", 4000}
 
 if Mix.env() == :test do
   import_config "#{Mix.env()}.exs"
