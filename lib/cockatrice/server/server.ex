dc48183f05a77d14e92db16d812878d6d0012ccd
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-03-16 01:54:23 +0100

    feat/server: add tests for Cockatrice.Server

--- lib/cockatrice/server/server.ex
+++ lib/cockatrice/server/server.ex
@@ -3,12 +3,12 @@ defmodule Cockatrice.Server do
   alias Cockatrice.Server.Router
   require Logger
 
-  def start(_type, _args) do
-    children = [
-      Cowboy.child_spec(scheme: :http, plug: Router, options: [port: 4001])
-    ]
+  def children() do
+    [Cowboy.child_spec(scheme: :http, plug: Router, options: [port: 4001])]
+  end
 
+  def start(_type, _args) do
     Logger.info("Starting Cockatrice on http://localhost:4001")
-    Supervisor.start_link(children, strategy: :one_for_one)
+    Supervisor.start_link(children(), strategy: :one_for_one)
   end
 end
--- test/server/server_test.exs
+++ test/server/server_test.exs
@@ -1,3 +1,23 @@
 defmodule CockatriceTest.Server do
   use ExUnit.Case
+  import Dummy
+
+  alias Cockatrice.Server
+
+  test "the children function" do
+    dummy Plug.Cowboy, ["child_spec"] do
+      result = [
+        [scheme: :http, plug: Cockatrice.Server.Router, options: [port: 4001]]
+      ]
+
+      assert Server.children() == result
+    end
+  end
+
+  test "the start function" do
+    dummy Supervisor, [{"start_link", fn x, y -> [x, y] end}] do
+      result = Supervisor.start_link(Server.children(), strategy: :one_for_one)
+      assert Server.start("", "") == result
+    end
+  end
 end
