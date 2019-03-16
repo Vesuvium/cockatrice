40f5beb8aa10384f6784991cd9fc3b1917a48c24
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-03-16 19:59:08 +0100

    chore/config: add more config options

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,8 @@ use Mix.Config
 
 config :cockatrice,
   dist: {:system, "COCKATRICE_DIST_FOLDER", "dist"},
+  content: {:system, "COCKATRICE_CONTENT_FOLDER", "content"},
+  templates: {:system, "COCKATRICE_TEMPLATES_FOLDER", "templates"},
   port: {:system, "COCKATRICE_PORT", 4000}
 
 if Mix.env() == :test do
