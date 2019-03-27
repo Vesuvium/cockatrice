875028b5e8d19b5ed88856c267251c20431c835e
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-03-27 09:10:11 +0100

    feat/page: add new function

--- lib/cockatrice/page.ex
+++ lib/cockatrice/page.ex
@@ -1,7 +1,20 @@
 defmodule Cockatrice.Page do
+  @templating_engine Cockatrice.Adapters.Pug
+  alias Cockatrice.Markdown
+
   @moduledoc """
   Creates and writes pages
   """
+
+  def new(file) do
+    templates_folder = Confex.get_env(:cockatrice, :templates)
+    config = Application.get_env(:cockatrice, __MODULE__, [])
+    engine = config[:templating_engine] || @templating_engine
+
+    file
+    |> Markdown.read()
+    |> engine.compile("#{templates_folder}/layout.pug")
+  end
 
   def write(page, dist_folder) do
     File.write("#{dist_folder}/index.html", page)
--- test/unit/page_test.exs
+++ test/unit/page_test.exs
@@ -2,6 +2,22 @@ defmodule CockatriceTest.Page do
   use ExUnit.Case
   import Dummy
   alias Cockatrice.Page
+  alias Cockatrice.Markdown
+  alias Cockatrice.Adapters.Pug
+
+  test "the new function" do
+    dummy Confex, [{"get_env", fn _a, _b -> "templates" end}] do
+      dummy Markdown, ["read"] do
+        dummy Pug, [{"compile", fn _a, _b -> nil end}] do
+          Page.new("file")
+          assert called(Confex.get_env(:cockatrice, :templates))
+          assert called(Markdown.read("file"))
+          assert called(Pug.compile("file", "templates/layout.pug"))
+        end
+      end
+    end
+  end
+
   test "the write function" do
     dummy File, [{"write", fn _a, _b -> nil end}] do
       Page.write("page", "dist")
