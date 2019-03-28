eecf3f720b1b4d571cf5717b8c8b785077d8467a
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-03-28 22:15:16 +0100

    feat/config: add default templating engine

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,8 @@ config :cockatrice,
   dist: {:system, "COCKATRICE_DIST_FOLDER", "dist"},
   content: {:system, "COCKATRICE_CONTENT_FOLDER", "content"},
   templates: {:system, "COCKATRICE_TEMPLATES_FOLDER", "templates"},
-  port: {:system, "COCKATRICE_PORT", 4000}
+  port: {:system, "COCKATRICE_PORT", 4000},
+  templating_engine: Cockatrice.Adapters.Pug
 
 if Mix.env() == :test do
   import_config "#{Mix.env()}.exs"
