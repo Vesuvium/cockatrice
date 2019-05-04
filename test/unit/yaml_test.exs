3a20083a7bcb3e7b68525d250000b83488bec809
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-05-04 18:02:52 +0200

    chore/tests: move yaml.exs to yaml_test.exs

--- test/unit/yaml_test.exs
+++ test/unit/yaml_test.exs
@@ -4,7 +4,7 @@ defmodule MedusaTest.Yaml do
   alias Medusa.Yaml
 
   test "reading yaml files successfully" do
-    dummy YamlElixir, [{"read_from_string", {:ok, string}}] do
+    dummy YamlElixir, [{"read_from_string", {:ok, "string"}}] do
       assert Yaml.read("string") == "string"
       called(YamlElixir.read_from_string("string"))
     end
